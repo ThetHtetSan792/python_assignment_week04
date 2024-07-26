d4a11d237ab0d7c19956e0caf19536caef50a594
ThetHtetSan792 <thethtetsan792@gmail.com>
2024-07-26 12:04:59 +0700

    updated

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1 / num2
+
+
+print(add(2, 4))
+print(subtract(2, 4))
+print(multiply(2, 4))
+print(divide(2, 4))
---
+++ lambda_calculator.py
@@ -0,0 +1,9 @@
+add = lambda num1, num2: num1 + num2
+subtract = lambda num1, num2: num1 - num2
+multiply = lambda num1, num2: num1 * num2
+divide = lambda num1, num2: num1 / num2
+
+print(add(2, 4))
+print(subtract(2, 4))
+print(multiply(2, 4))
+print(divide(2, 4))
